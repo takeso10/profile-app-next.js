4e076f46ab440b2d937c1eba3e00206c79576c1d
takeso10 <sotatake7716@gmail.com>
2023-06-28 11:43:17 +0900

    private,productsページ作成

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,33 +1,48 @@
 import Link from "next/link";
-import Image from 'next/image'
-import styles from '../styles/header.module.scss'
+import Image from "next/image";
+import styles from "../styles/header.module.scss";
 
-export default function Header (){
-    return(
-        <div  className={styles.header}>
-            <Link href='/' className={styles.home}>
-                HOME
-            </Link>
-            <div className={styles.category}>
-                <Link href='./profile' className={styles.other}>
-                   PROFILE
-                </Link>
-                <Link href='./job' className={styles.other}>
-                    WORKS
-                </Link>
-                <Link href='./private' className={styles.other}>
-                    PRIVATE
-                </Link>
-                <Link href='./products' className={styles.other}>
-                    PRODUCTS
-                </Link>
-                <Link href={'https://twitter.com/QHXxUXYerj4pTcq'} className={styles.twitter_link}>
-                    <Image src="/twitter.png" alt ="twitter logo" width={25} height={25} className={styles.logo}></Image>
-                </Link>
-                <Link href={"https://www.instagram.com/sota_108/"}>
-                    <Image src="/Instagram_logo_2022.svg.webp" alt ="insta logo" width={25} height={25} className={styles.logo}></Image>
-                </Link>
-            </div>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div className={styles.header}>
+      <Link href="/" className={styles.home}>
+        HOME
+      </Link>
+      <div className={styles.category}>
+        <Link href="./profile" className={styles.other}>
+          PROFILE
+        </Link>
+        <Link href="./job" className={styles.other}>
+          WORKS
+        </Link>
+        <Link href="./private" className={styles.other}>
+          PRIVATE
+        </Link>
+        <Link href="./products" className={styles.other}>
+          PRODUCTS
+        </Link>
+        <Link
+          href={"https://twitter.com/QHXxUXYerj4pTcq"}
+          className={styles.twitter_link}
+        >
+          <Image
+            src="/twitter.png"
+            alt="twitter logo"
+            width={25}
+            height={25}
+            className={styles.logo}
+          ></Image>
+        </Link>
+        <Link href={"https://www.instagram.com/sota_108/"}>
+          <Image
+            src="/Instagram_logo_2022.svg.webp"
+            alt="insta logo"
+            width={25}
+            height={25}
+            className={styles.logo}
+          ></Image>
+        </Link>
+      </div>
+    </div>
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,9 +1,50 @@
+import Link from "next/link";
 import Header from "../../../components/Header";
+import Image from "next/image";
+import styles from "../../../styles/products.module.scss";
 
-export default function Products (){
-    return(
-        <div>
-            <Header></Header>
+
+export default function Products() {
+  return (
+    <div>
+      <Header></Header>
+      <div className={styles.main}>
+        <div className={styles.github_account}>
+        <Link
+          href={"https://github.com/takeso10"}
+          className={styles.github_link}
+        >
+          <Image
+            src="/github.svg.png"
+            alt="github logo"
+            width={50}
+            height={50}
+            className={styles.logo}
+          ></Image>
+        </Link>
+        <a href={"https://github.com/takeso10"}>私のGitHubアカウント</a>
         </div>
-    )
+        <div className={styles.products}>
+          <div className={styles.product}>
+          <h2>ゆめみフロントエンドテスト</h2>
+          <div className={styles.link}>
+            <Link href={"https://yumemi-frontend-coding-test-wheat.vercel.app/"}>https://yumemi-frontend-coding-test-wheat.vercel.app/</Link>
+          </div>
+          <Image
+            src="/yumemi_test.png" alt={"yumemi test"} width={800} height={500}
+          />
+          </div>
+          <div className={styles.product}>
+            <h2>スノーボードギアレビューサイト</h2>
+            <div className={styles.link}>
+              <Link href={"https://snowboard-gear-review-app.vercel.app/"}>https://snowboard-gear-review-app.vercel.app/</Link>              
+            </div>
+          <Image
+            src="/snowgear.png" alt={"snowgear test"} width={800} height={500}
+          />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
